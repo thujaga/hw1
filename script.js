c5ea3f21aa3aa192e5623506d53b5039e2450ec3
Mary <vovaletochka@gmail.com>
2021-04-04 20:21:05 +0300

    hw/5

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-var num = prompt('number');
-console.log(num);
-var string = '';
-for (let i = 0; i <= num; i++) {
- var snows = '';
- for (let q = 0; q < i; q++) {
-  snows += '*'
+console.log(padString('hello', '*', 10, true));
+console.log(padString('hello', '*', 10, false));
+console.log(padString('hello', '*', 3));
+console.log(padString('world', '*', 5));
+// console.log(padString('world', '*', 2));
+
+function padString(string, char, number, left=true) {
+ var charsCount = string.length;
+ var length = number-charsCount;
+
+ if (length > 0) {
+  var newString = '';
+  for (let i = 0; i<length; i++){
+   newString+=char
+  }
+  if (left){
+   return newString+string;
+  } else {
+   return string+newString;
+  }
+ } else {
+  return string
  }
- string += snows + "\n"
 }
-console.log(string);
